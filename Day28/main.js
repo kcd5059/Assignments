e44fcf4c35ec40ee3a382fa828efd0ecafcadcc6
Kyle <kcd5059@gmail.com>
2016-09-21 16:36:25 -0400

    Adds comments to Day28

--- Day28/main.js
+++ Day28/main.js
@@ -7,25 +7,27 @@ $(document).ready(function() {
   var decButton = $("#decrement");
   var resButton = $("#reset");
   var proButton = $("#pro-button");
-  var modButton = $("#mod-button");
   var heading = $("h1");
   var pro = false;
   var modifier = 1;
   var modifierInput = $("#mod-input");
 
-
+    // Display initial value of number
     numberLocation.text(number);
 
+    // When + button is clicked
     incButton.click(function() {
       generateNumberPlus();
       numberLocation.text(number);
       updateStatus();
     });
+    // When - button is clicked...
     decButton.click(function() {
       generateNumberMinus();
       numberLocation.text(number);
     updateStatus();
     });
+    // When reset button is clicked...
     resButton.click(function() {
       number = 0;
       numberLocation.text(number);
@@ -36,10 +38,6 @@ $(document).ready(function() {
       modifier = 1;
       modifierInput.val("1");
     });
-    modButton.click(function() {
-      modifier = Number(modifierInput.val());
-      console.log(modifier);
-    });
     // Toggle between pro version of counter
     proButton.click(function() {
       if(pro) {
@@ -47,19 +45,17 @@ $(document).ready(function() {
         pro = false;
         modifierInput.val("1");
         modifier = 1;
-        modButton.removeAttr("disabled");
         modifierInput.removeAttr("disabled");
       } else {
         proButton.text("Deactivate Pro");
         pro = true;
         modifierInput.val("Random")
-        modButton.attr("disabled", "disabled");
         modifierInput.attr("disabled", "disabled");
       }
 
     });
 
-    // Generate number between -10 and 10 or increment
+    // Generate number between -10 and 10 or increment/decrement
     function generateNumberPlus() {
       if(pro) {
         number = Math.floor(Math.random() * 21) - 10;
@@ -78,6 +74,7 @@ $(document).ready(function() {
     }
 
 
+    // Updates various elements on the page after clicks
     function updateStatus() {
       // Generates a random background color for the number box
       var color = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
